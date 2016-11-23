3c8474a1cac1e1943837bde6561f835be7f7da8a
Marin Onofrei <onofrei.marin@gmail.com>
2016-11-23 20:46:42 +0200

    Added project placeholder

---
+++ src/main/java/org/testmonkeys/jwait/App.java
@@ -0,0 +1,8 @@
+package org.testmonkeys.jwait;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("App");
+    }
+}
---
+++ src/test/java/org/testmonkeys/jwait/test/SampleTest.java
@@ -0,0 +1,11 @@
+package org.testmonkeys.jwait.test;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void test(){
+        assert true;
+    }
+}
